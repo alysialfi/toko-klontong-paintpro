8f4697940b6ab6283ac31cc397b57acb1c71c4b3
alysialfi <9c.alysialfi@gmail.com>
2025-10-17 01:29:50 +0700

    initial: installation and setup

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>Tes</p>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,30 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/test-utils',
+    'shadcn-nuxt',
+    '@pinia/nuxt',
+  ],
+  css: ['~/assets/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+})
