baf204d748b1fd561556bf98be5f9fbabd2a87c2
alysialfi <9c.alysialfi@gmail.com>
2025-10-17 17:14:42 +0700

    feat(models): provide Product model

---
+++ app/models/Product.ts
@@ -0,0 +1,14 @@
+export type Product = {
+  id: number;
+  CategoryId: number;
+  categoryName: string;
+  sku: string;
+  name: string;
+  description: string;
+  weight: number;
+  width: number;
+  length: number;
+  height: number;
+  image: string;
+  price: number;
+};
