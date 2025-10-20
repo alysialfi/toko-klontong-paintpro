1ccd6c883c69d8d0a9bd96be0e508bf710600d06
alysialfi <9c.alysialfi@gmail.com>
2025-10-20 23:54:02 +0700

    feat(utils): create debouncer to prevent laggy search

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -5,3 +5,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function useDebounce<T extends (...args: any[]) => void>(
+  fn: T,
+  delay: number
+) {
+  let timeoutId: ReturnType<typeof setTimeout>
+
+  return function (...args: Parameters<T>) {
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(() => fn(...args), delay)
+  }
+}
